2cde4386ab0c471409c2343c661e34c02080a745
talentpoolai <talentpool.admin@thetalentpool.ai>
2025-07-20 02:03:12 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ export default {
         }
       },
       fontFamily: {
-        'sans': ['Inter', 'system-ui', 'sans-serif'],
+        'Roboto': ['Inter', 'system-ui', 'sans-serif'],
       },
       animation: {
         'fade-in': 'fadeIn 0.6s ease-out',
